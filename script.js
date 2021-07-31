8d540eea96fb404c6046ab591314dc7d06e4efbc
dinara994 <di_94@mail.ru>
2021-07-31 22:40:51 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const r1 = document.querySelector('#r1')
+const offsetX = document.querySelector('#offsetX')
+const offsetY = document.querySelector('#offsetY')
+const blur = document.querySelector('#blur')
+const spread = document.querySelector('#spread')
+const  color = document.querySelector('#color')
+const  colorBox = document.querySelector('#colorBox')
+const box = document.querySelector('.box')
+const out = document.querySelector('.out')
+const inset = document.querySelector('#inset')
+const outset = document.querySelector('#outset')
+let result = document.querySelector('#result' )
+
+
+
+//r1.onchange = generateBoxShadow
+r1.oninput = generateBoxShadow
+offsetX.oninput = generateBoxShadow
+offsetY.oninput = generateBoxShadow
+blur.oninput = generateBoxShadow
+spread.oninput = generateBoxShadow
+color.oninput = generateBoxShadow
+colorBox.oninput = colorBoxResult
+inset.oninput = generateBoxShadow
+outset.oninput = generateBoxShadow
+
+
+
+function generateBoxShadow() {
+    box.style.borderRadius = r1.value + '%'
+    box.style.boxShadow = `${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} inset `
+    out.innerHTML = `box-shadow: ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} </br>`
+    out.innerHTML += `-webkit-box-shadow ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} </br>`
+    out.innerHTML = `-moz-box-shadow ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value}`
+
+    if (inset.checked) {
+
+    }
+}
+
+//Создали функцию для color box
+function colorBoxResult () {
+    box.style.backgroundColor = colorBoxResult.value
+}
+
+
+
+
+
+
+//innerHtml , br
+
+
+
+
+
+
+
+
